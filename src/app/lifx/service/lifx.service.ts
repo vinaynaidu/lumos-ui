cdc9541730d73b380c85239ca74ab403aa276c69
Vinay Naidu <vinay.naidu@gmail.com>
2018-04-25 18:48:52 +0100

    added shell methods

--- src/app/lifx/service/lifx.service.ts
+++ src/app/lifx/service/lifx.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LifxService {
 
   constructor() { }
+
+  getAllBulbs(): Observable<any> {
+    // TODO: Get list of bulbs
+    return Observable.of([]);
+  }
+
+  getBulbStatus(): Observable<boolean> {
+    // TODO: get bulb status for all bulbs
+    return Observable.of(true);
+  }
+
+  getBulbStatusByBulb(bulbId: number): Observable<any> {
+    // TODO: Get bulb status for one bulb
+    return Observable.of({});
+  }
+
+  setBulbColour(): void {
+    // TODO: Set bulb colour
+  }
+
+  setBulbStatus(isBulbSwitchedOn: boolean): void {
+    // TODO: Power on / off bulb
+  }
 
 }
